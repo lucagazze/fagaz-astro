8b7dc95b88e4e74d5f6ec85f128fd7e24d1757ea
lucagazze <lucagazze1@gmail.com>
2026-01-22 13:00:52 -0300

    2

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
