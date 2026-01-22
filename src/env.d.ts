2cf82bbf48571c44d47323a0a409d0832bc05a7f
lucagazze <lucagazze1@gmail.com>
2026-01-22 15:16:52 -0300

    2

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,12 @@
 /// <reference path="../.astro/types.d.ts" />
+
+interface ImportMetaEnv {
+  readonly SMTP_HOST: string;
+  readonly SMTP_PORT: string;
+  readonly SMTP_USER: string;
+  readonly SMTP_PASS: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
